83df36491ab14397882269d1bfccf23948f60011
Berry  Semexan <bsemexan@gmail.com>
2018-02-19 03:02:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,11 +4,8 @@ class School
   attr_accessor :add_student, :grade, :roster
   attr_reader :name
 
-  ROSTER = {}
-
   def initialize(name)
     @name = name
-    ROSTER << name
   end
 
   def grade
